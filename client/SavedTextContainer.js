2948a879a683a04e5ba1953560b33f5963d176bd
JonFerraiolo <jon@ferraiolo.com>
2019-06-14 13:11:26 -0700

    First nested control with hyperHTML!

--- client/SavedTextContainer.js
+++ client/SavedTextContainer.js
@@ -1,12 +1,11 @@
 
-/**
-
-*/
-
+import { SavedTextControl } from './SavedTextControl.js'
 const { wire } = hyperHTML;
 
 export function SavedTextContainer(text) {
   let obj = { text };
+  //return wire()`
+  //<div class=SavedTextContainer>${obj.text}</div>`;
   return wire()`
-  <div class=SavedTextContainer>${obj.text}</div>`;
+  <div class=SavedTextContainer>${SavedTextControl(obj.text)}</div>`;
 }
