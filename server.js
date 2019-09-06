8fab207f16e66211d1b200e85ebd543a23ff5846
JonFerraiolo <jon@ferraiolo.com>
2019-09-06 16:23:30 -0700

    redirect http to https on real server

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const appserver = process.env.APPSERVER || 'localhost';
 const port = process.env.PORT || 3000;
 const rootDir = process.cwd();
 
 app.use(express.static('client'));
-app.get('/', (req, res) => res.sendFile(rootDir+'/client/app.html'));
+app.get('/', (req, res) => {
+  if (appserver === 'eyevocalize.com' && req.protocol === 'http') {
+    res.redirect('https://' + req.headers.host);
+  } else {
+    res.sendFile(rootDir+'/client/app.html');
+  }
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`App listening on port ${port}!`));
