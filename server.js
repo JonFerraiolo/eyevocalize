cf6632ad3050484d2e54d2e7cf8a7cdeeb1c3d5b
JonFerraiolo <jonferraiolo@gmail.com>
2019-09-06 20:40:10 -0400

    redirect failed. reverting server.js

--- server.js
+++ server.js
@@ -5,12 +5,6 @@ const port = process.env.PORT || 3000;
 const rootDir = process.cwd();
 
 app.use(express.static('client'));
-app.get('/', (req, res) => {
-  if (appserver === 'eyevocalize.com' && req.protocol === 'http') {
-    res.redirect('https://' + req.headers.host);
-  } else {
-    res.sendFile(rootDir+'/client/app.html');
-  }
-});
+app.get('/', (req, res) => res.sendFile(rootDir+'/client/app.html'));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
