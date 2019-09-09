aac7eecc91d31def72a3e40b7c7f4abd2bf03049
JonFerraiolo <jon@ferraiolo.com>
2019-09-09 09:50:15 -0700

    move index.js to server/server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const appserver = process.env.EVC_APPSERVER || 'localhost';
 const port = process.env.EVC_PORT || 3000;
 const rootDir = process.cwd();
 
-app.use(express.static('client'));
-app.get('/', (req, res) => res.sendFile(rootDir+'/client/app.html'));
+app.use(express.static(path.normalize(rootDir+'/../client')));
+app.get('/', (req, res) => res.sendFile(path.normalize(rootDir+'/../client/app.html')));
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
