a70908bda78977a90045f8913e256d99716f87eb
JonFerraiolo <jon@ferraiolo.com>
2019-09-09 15:35:53 -0700

    revert back a few commitssxxRevert "update package.json for all modules I think we need, but none of the new are working yet."
    This reverts commit ed9352248e51c1a565a8450e82ae97830e5619c0.

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const app = express();
+const appserver = process.env.EVC_APPSERVER || 'localhost';
+const port = process.env.EVC_PORT || 3000;
+const rootDir = process.cwd();
+
+app.use(express.static('client'));
+app.get('/', (req, res) => res.sendFile(rootDir+'/client/app.html'));
+
+app.listen(port, () => console.log(`App listening on port ${port}!`));
+
+/*
+const envvars = require('./server/envvars');
+envvars.check(); // exits if any env vars not set
+const express = require("express");
+const bodyParser = require('body-parser');
+const dbconnection = require('./server/dbconnection');
+const sessionMgmt = require('./server/sessionMgmt');
+const sessionRoutes = require('./server/sessionRoutes');
+const dataRoutes = require('./server/dataRoutes');
+const hostname = process.env.EVC_HOSTNAME || 'localhost';
+const port = process.env.EVC_PORT || 3000;
+const rootDir = process.cwd();
+const connection = dbconnection.getConnection();
+const app = express();
+// use something else, maybe debug. Look at package.json
+//app.use(function(req, res, next) {
+  //console.log('req.path='+req.path)
+  //next()
+//});
+const authMiddleware = sessionMgmt.auth;
+sessionMgmt.init(app, connection); // calls app.use with session middleware
+app.use(bodyParser.urlencoded({ extended: true })); // for uploading files
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+// need to study this. Don't need CORS. Maybe all is for CORS
+  res.header("Access-Control-Allow-Origin", TEAM_CORS_ALLOWED_DOMAIN);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cookie");
+  next();
+});
+app.use(express.static('client'));
+app.get('/', (req, res) => res.sendFile(rootDir+'/client/app.html'));
+const apiRouter = express.Router();
+apiRouter.post('/signup', sessionRoutes.signup)
+apiRouter.post('/login', sessionRoutes.login)
+apiRouter.post('/logout', sessionRoutes.logout)
+apiRouter.post('/loginexists', sessionRoutes.loginexists)
+apiRouter.post('/resendverification', sessionRoutes.resendVerificationEmail)
+apiRouter.get('/verifyaccount/:token', sessionRoutes.verifyAccount)
+apiRouter.post('/sendresetpassword', sessionRoutes.sendResetPasswordEmail)
+apiRouter.get('/gotoresetpasswordpage/:token', sessionRoutes.gotoResetPasswordPage)
+apiRouter.post('/resetpassword', sessionRoutes.resetPassword)
+apiRouter.post('/updatedata', authMiddleware, dataRoutes.updatedata)
+app.use(apiRouter);
+app.listen(port);
+*/
